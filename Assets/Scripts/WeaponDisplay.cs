01c1bbacde56dd96956f60a4b5b1828fcb910ae2
Jonathan Ritter <jonritter1995@gmail.com>
2017-06-17 16:08:18 -0400

    Added bullet fire for all guns

---
+++ Assets/Scripts/WeaponDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponDisplay : MonoBehaviour {
+
+	private Image currentWeapon;
+	private WeaponManager wm;
+
+	public Sprite[] weaponSprites;
+
+	// Use this for initialization
+	void Start () {
+		currentWeapon = GetComponent<Image> ();
+		wm = GameObject.Find ("Player").GetComponent<WeaponManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentWeapon.sprite = weaponSprites [wm.currentGun];
+	}
+}
--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -4,24 +4,96 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour {
 
-	public const int PISTOL = 0, RIFLE = 1, SHOTGUN = 2;
-	public readonly int[] CLIP_SIZES = { 7, 30, 5 }; 
+	public const int PISTOL = 0, RIFLE = 1, SHOTGUN = 2;	// id's for weapons.
+	public readonly int[] CLIP_SIZES = { 7, 30, 5 }; 		// max ammo per clip.
+	public readonly int[] FIRE_RATES = { 20, 10, 45 };		// min # of frames between shots.
+	public const int WEAPON_CHANGE_SPEED = 60;				// min # of frames it takes to switch weapons.
+	public readonly int[] RELOAD_SPEEDS = { 60, 100, 140 };
 
-	public int currentGun;
-	public int[] currentAmmo;
-	public int[] totalAmmo;
+	public int currentGun;									// The gun the player is currently using.
+	public int[] currentAmmo;								// The ammount of ammo in the current clip.
+	public int[] totalAmmo;									// The amount of ammo not in the current clip.
+
+	private bool firing;
+	private int cdCounter;
+	private int reloadCounter;
+
+	public GameObject[] bullets;
+	public GameObject[] bulletLocations;
+	private Transform playerTransform;
 
 
 	// Use this for initialization
 	void Start () {
-		currentAmmo = new int[3];
-		totalAmmo = new int[3];
-		currentAmmo [0] = CLIP_SIZES [0];
-		totalAmmo [0] = 21;
+		playerTransform = GetComponent<Transform> ();
+		cdCounter = 1000;
+		reloadCounter = 1000;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+			
+	}
+
+	void FixedUpdate() {
 		
+		// Stop incrementing to avoid overflow.
+		if (cdCounter < 10000)
+			cdCounter++;
+
+		if (reloadCounter < 10000)
+			reloadCounter++;
+
+		if (reloadCounter < RELOAD_SPEEDS [currentGun]) {
+			return;
+		}
+
+		// Clip empty.
+		if (firing && currentAmmo[currentGun] <= 0) {
+			Reload ();
+			return;
+		}
+
+		// Inbetween shots - not enough time since last bullet fired.
+		if (firing && cdCounter < FIRE_RATES [currentGun])
+			return;
+		
+		// fire projectile based on weapon
+		if (firing) {
+			cdCounter = 0;
+			currentAmmo [currentGun]--;
+
+			Quaternion bulletRot = Quaternion.Euler (new Vector3 (playerTransform.rotation.eulerAngles.x, playerTransform.rotation.eulerAngles.y, playerTransform.eulerAngles.z - 90));
+			Vector3 bulletLoc = bulletLocations [currentGun].GetComponent<Transform> ().position;
+			GameObject[] projectiles = (currentGun == SHOTGUN) ? new GameObject[3] : new GameObject[1];
+
+			for (int i = 0; i < projectiles.Length; i++) {
+				projectiles[i] = Instantiate (bullets [currentGun], bulletLoc, bulletRot ) as GameObject;
+				if (i > 0) {
+					projectiles [i].GetComponent<Transform> ().position = projectiles [0].GetComponentsInChildren<Transform> () [i].position;
+					projectiles [i].GetComponent<Transform> ().rotation = projectiles [0].GetComponentsInChildren<Transform> () [i].rotation;
+				}
+				projectiles[i].GetComponent<Rigidbody2D> ().velocity = projectiles[i].transform.up * 10;
+			}
+		}
+	}
+
+	private void Reload() {
+		reloadCounter = 0;
+		if (totalAmmo [currentGun] >= CLIP_SIZES [currentGun]) {
+			totalAmmo [currentGun] -= CLIP_SIZES [currentGun];
+			currentAmmo [currentGun] = CLIP_SIZES [currentGun];
+		} else if (totalAmmo [currentGun] > 0) {
+			currentAmmo [currentGun] = totalAmmo[currentGun];
+			totalAmmo [currentGun] = 0;
+		}
+	}
+
+	public void PointerDown() {
+		firing = true;	
+	}
+
+	public void PointerUp() {
+		firing = false;
 	}
 }
